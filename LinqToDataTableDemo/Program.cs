f2e060b6fd24762c428c69540c0b6636d0a6839f
shashiRgowda <shashikumartr64@gmail.com>
2021-06-13 19:58:31 +0530

    Added:Linq to Data Table

---
+++ LinqToDataTableDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LinqToDataTableDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data Table demo");
+            LinqToDataTable linqToDataTable = new LinqToDataTable();
+            linqToDataTable.AddToDataTableDemo();
+            Console.ReadKey();
+
+        }
+    }
+}
